425fb8130ad1a6acd8fbe78e81ff8c5603faa610
Mark Donatelli <markwdonatelli@yahoo.com>
2022-06-17 10:35:25 -0400

    header and  initialsetup

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,16 +1,41 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export const useCounterStore = defineStore({
   id: 'counter',
   state: () => ({
-    counter: 0
+    counter: 0,
+    gigs: [
+      {
+        venue: '',
+        amount: 200,
+      },
+      {
+        venue: '',
+        amount: 200,
+      },
+      {
+        venue: '',
+        amount: 200,
+      },
+      {
+        venue: '',
+        amount: 200,
+      },
+    ],
   }),
   getters: {
-    doubleCount: (state) => state.counter * 2
+    doubleCount: (state) => state.counter * 2,
+    getTotal: (state) => {
+      let total = 0;
+      state.gigs.forEach((item) => {
+        total += item.amount;
+      });
+      return total;
+    },
   },
   actions: {
     increment() {
-      this.counter++
-    }
-  }
-})
+      this.counter++;
+    },
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Darker Grotesque', 'sans-serif'],
+      },
+      colors: {
+        pink: '#FF90E7',
+        yellow: '#FFC902',
+        green: '#22A094',
+      },
+    },
   },
   plugins: [],
 };
